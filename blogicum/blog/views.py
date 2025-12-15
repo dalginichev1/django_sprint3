5bb87ab63c5b60f39cccd729799fe1b1a907b9f3
dalginichev1@bk.ru <daalginichev@DESKTOP-9VBGCR6.localdomain>
2025-12-16 00:48:06 +0300

    v2

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -12,7 +12,7 @@ def index(request):
         category__is_published=True,
         pub_date__lte=timezone.now()
     ).order_by('-pub_date')[:5]
-    
+
     context = {'post_list': post_list}
     return render(request, template, context)
 
@@ -24,7 +24,7 @@ def category_posts(request, category_slug):
         slug=category_slug,
         is_published=True
     )
-    
+
     post_list = Post.objects.select_related(
         'category', 'location', 'author'
     ).filter(
@@ -32,7 +32,7 @@ def category_posts(request, category_slug):
         is_published=True,
         pub_date__lte=timezone.now()
     ).order_by('-pub_date')
-    
+
     context = {
         'category': category,
         'post_list': post_list
@@ -42,7 +42,7 @@ def category_posts(request, category_slug):
 
 def post_detail(request, post_id):
     template = 'blog/detail.html'
-    
+
     post = get_object_or_404(
         Post.objects.select_related('category', 'location', 'author'),
         pk=post_id,
@@ -50,6 +50,6 @@ def post_detail(request, post_id):
         category__is_published=True,
         pub_date__lte=timezone.now()
     )
-    
+
     context = {'post': post}
     return render(request, template, context)
